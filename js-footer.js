79bc28aa8c4d95ecd72692b6f74becb6b2d659c6
REMusic99 <bfdcsd@gmail.com>
2024-12-27 11:33:46 -0700

    Add files via upload

---
+++ js-footer.js
@@ -0,0 +1,7 @@
+
+
+        document.write("<div class='col-12 section-1'><div class='col-6';><a href='index.html'><img src='images/img-Phosphate RE.png'></a></div><div class='col-6' style='color: #ff6666';>Copyright © 2012-2024<br>raggedyedgemusic.com</div></div>");
+        
+          /*<a href='index.html'><img src='images/pageheader-RaggedyEdge.png'></a>
+        
+      style='background-image: url('images/footer-notes.jpg')*/
